f9ea32f49e4361d397db9c397e68d54d604abfc8
blacroix <benjlacroix@gmail.com>
2013-12-22 20:56:12 +0100

    Use rhcloud serverjs

--- server.js
+++ server.js
@@ -1,244 +1,124 @@
-#!/usr/bin/env node
-
-var util = require('util'),
-    http = require('http'),
-    fs = require('fs'),
-    url = require('url'),
-    events = require('events');
-
-var DEFAULT_PORT = 8000;
-
-function main(argv) {
-    new HttpServer({
-        'GET': createServlet(StaticServlet),
-        'HEAD': createServlet(StaticServlet)
-    }).start(Number(argv[2]) || DEFAULT_PORT);
-}
-
-function escapeHtml(value) {
-    return value.toString().
-        replace('<', '&lt;').
-        replace('>', '&gt;').
-        replace('"', '&quot;');
-}
-
-function createServlet(Class) {
-    var servlet = new Class();
-    return servlet.handleRequest.bind(servlet);
-}
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
 
 /**
- * An Http server implementation that uses a map of methods to decide
- * action routing.
- *
- * @param {Object} Map of method => Handler function
+ *  Define the sample application.
  */
-function HttpServer(handlers) {
-    this.handlers = handlers;
-    this.server = http.createServer(this.handleRequest_.bind(this));
-}
+var MosaicIT = function() {
 
-HttpServer.prototype.start = function(port) {
-    this.port = port;
-    this.server.listen(port);
-    util.puts('Http Server running at http://localhost:' + port + '/');
-};
-
-HttpServer.prototype.parseUrl_ = function(urlString) {
-    var parsed = url.parse(urlString);
-    parsed.pathname = url.resolve('/', parsed.pathname);
-    return url.parse(url.format(parsed), true);
-};
-
-HttpServer.prototype.handleRequest_ = function(req, res) {
-    var logEntry = req.method + ' ' + req.url;
-    if (req.headers['user-agent']) {
-        logEntry += ' ' + req.headers['user-agent'];
-    }
-    util.puts(logEntry);
-    req.url = this.parseUrl_(req.url);
-    var handler = this.handlers[req.method];
-    if (!handler) {
-        res.writeHead(501);
-        res.end();
-    } else {
-        handler.call(this, req, res);
-    }
-};
-
-/**
- * Handles static content.
- */
-function StaticServlet() {}
-
-StaticServlet.MimeMap = {
-    'txt': 'text/plain',
-    'html': 'text/html',
-    'css': 'text/css',
-    'xml': 'application/xml',
-    'json': 'application/json',
-    'js': 'application/javascript',
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'gif': 'image/gif',
-    'png': 'image/png',
-    'svg': 'image/svg+xml'
-};
-
-StaticServlet.prototype.handleRequest = function(req, res) {
+    //  Scope.
     var self = this;
-    var path = ('./' + req.url.pathname).replace('//','/').replace(/%(..)/g, function(match, hex){
-        return String.fromCharCode(parseInt(hex, 16));
-    });
-    var parts = path.split('/');
-    if (parts[parts.length-1].charAt(0) === '.')
-        return self.sendForbidden_(req, res, path);
-    fs.stat(path, function(err, stat) {
-        if (err)
-            return self.sendMissing_(req, res, path);
-        if (stat.isDirectory())
-            return self.sendDirectory_(req, res, path);
-        return self.sendFile_(req, res, path);
-    });
-}
 
-StaticServlet.prototype.sendError_ = function(req, res, error) {
-    res.writeHead(500, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<!doctype html>\n');
-    res.write('<title>Internal Server Error</title>\n');
-    res.write('<h1>Internal Server Error</h1>');
-    res.write('<pre>' + escapeHtml(util.inspect(error)) + '</pre>');
-    util.puts('500 Internal Server Error');
-    util.puts(util.inspect(error));
-};
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
 
-StaticServlet.prototype.sendMissing_ = function(req, res, path) {
-    path = path.substring(1);
-    res.writeHead(404, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<!doctype html>\n');
-    res.write('<title>404 Not Found</title>\n');
-    res.write('<h1>Not Found</h1>');
-    res.write(
-        '<p>The requested URL ' +
-            escapeHtml(path) +
-            ' was not found on this server.</p>'
-    );
-    res.end();
-    util.puts('404 Not Found: ' + path);
-};
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-StaticServlet.prototype.sendForbidden_ = function(req, res, path) {
-    path = path.substring(1);
-    res.writeHead(403, {
-        'Content-Type': 'text/html'
-    });
-    res.write('<!doctype html>\n');
-    res.write('<title>403 Forbidden</title>\n');
-    res.write('<h1>Forbidden</h1>');
-    res.write(
-        '<p>You do not have permission to access ' +
-            escapeHtml(path) + ' on this server.</p>'
-    );
-    res.end();
-    util.puts('403 Forbidden: ' + path);
-};
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
 
-StaticServlet.prototype.sendRedirect_ = function(req, res, redirectUrl) {
-    res.writeHead(301, {
-        'Content-Type': 'text/html',
-        'Location': redirectUrl
-    });
-    res.write('<!doctype html>\n');
-    res.write('<title>301 Moved Permanently</title>\n');
-    res.write('<h1>Moved Permanently</h1>');
-    res.write(
-        '<p>The document has moved <a href="' +
-            redirectUrl +
-            '">here</a>.</p>'
-    );
-    res.end();
-    util.puts('301 Moved Permanently: ' + redirectUrl);
-};
-
-StaticServlet.prototype.sendFile_ = function(req, res, path) {
-    var self = this;
-    var file = fs.createReadStream(path);
-    res.writeHead(200, {
-        'Content-Type': StaticServlet.
-            MimeMap[path.split('.').pop()] || 'text/plain'
-    });
-    if (req.method === 'HEAD') {
-        res.end();
-    } else {
-        file.on('data', res.write.bind(res));
-        file.on('close', function() {
-            res.end();
-        });
-        file.on('error', function(error) {
-            self.sendError_(req, res, error);
-        });
-    }
-};
-
-StaticServlet.prototype.sendDirectory_ = function(req, res, path) {
-    var self = this;
-    if (path.match(/[^\/]$/)) {
-        req.url.pathname += '/';
-        var redirectUrl = url.format(url.parse(url.format(req.url)));
-        return self.sendRedirect_(req, res, redirectUrl);
-    }
-    fs.readdir(path, function(err, files) {
-        if (err)
-            return self.sendError_(req, res, error);
-
-        if (!files.length)
-            return self.writeDirectoryIndex_(req, res, path, []);
-
-        var remaining = files.length;
-        files.forEach(function(fileName, index) {
-            fs.stat(path + '/' + fileName, function(err, stat) {
-                if (err)
-                    return self.sendError_(req, res, err);
-                if (stat.isDirectory()) {
-                    files[index] = fileName + '/';
-                }
-                if (!(--remaining))
-                    return self.writeDirectoryIndex_(req, res, path, files);
-            });
-        });
-    });
-};
-
-StaticServlet.prototype.writeDirectoryIndex_ = function(req, res, path, files) {
-    path = path.substring(1);
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    if (req.method === 'HEAD') {
-        res.end();
-        return;
-    }
-    res.write('<!doctype html>\n');
-    res.write('<title>' + escapeHtml(path) + '</title>\n');
-    res.write('<style>\n');
-    res.write('  ol { list-style-type: none; font-size: 1.2em; }\n');
-    res.write('</style>\n');
-    res.write('<h1>Directory: ' + escapeHtml(path) + '</h1>');
-    res.write('<ol>');
-    files.forEach(function(fileName) {
-        if (fileName.charAt(0) !== '.') {
-            res.write('<li><a href="' +
-                escapeHtml(fileName) + '">' +
-                escapeHtml(fileName) + '</a></li>');
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating MosaicIT app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
         }
-    });
-    res.write('</ol>');
-    res.end();
-};
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
 
-// Must be last,
-main(process.argv);
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+    };
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();
+        self.app.use("/", express.static(__dirname + '/public'));
+
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  MosaicIT Application.  */
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new MosaicIT();
+zapp.initialize();
+zapp.start();
+
